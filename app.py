321b6c220b611099d808a505d82d24ac1b899d6d
lfrm80-ship-it <lfrm80@gmail.com>
2026-03-27 13:47:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -125,8 +125,14 @@ st.markdown("<div style='height:20px;'></div>", unsafe_allow_html=True)
 col_left, col_center, col_right = st.columns([1, 1, 1])
 
 with col_center:
-    st.markdown("<div style='margin-top:12px;'></div>", unsafe_allow_html=True)
-    st.image("HIC.png", width=90)
+    st.markdown(
+        """
+        <div style="margin-top:12px;">
+            <img src="HIC.png" width="90">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
     # Título centrado
     st.markdown(

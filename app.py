62b702433455f0fef712bb38109bf037c4c28c1f
lfrm80-ship-it <lfrm80@gmail.com>
2026-03-27 19:28:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -237,6 +237,8 @@ with tab_promos:
 with tab_registro:
     st.subheader("Registrar nueva promoción")
 
+    with st.form("form_registro", clear_on_submit=True):
+
     promo = st.text_input("Nombre de la Promoción", key="promo")
     hoteles = st.multiselect("Propiedad(es)", PROPERTIES, key="hoteles")
 
